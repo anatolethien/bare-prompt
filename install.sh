d3e35021b78049729bdd16bd2ef8486ac241f475
anatolethien <mail@anatolethien.eu>
2022-03-02 19:43:54 +0100

    Script

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Script written by @anatolethien
+
+echo -e '\n# Prompt\nfunction prompt() {\n\tPS1=" \\e[1;36m\\w\\e[m\\n `if [ \$? -eq 0 ]; then echo "\\e[1;32m"; else echo "\\e[1;31m"; fi`❱\\e[m ";\n}\n' >> ~/.bashrc
+source ~/.bashrc
