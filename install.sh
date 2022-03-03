c16899b3ee4af163e65c43e48c0035f36acf8b70
anatolethien <mail@anatolethien.eu>
2022-03-03 10:41:47 +0100

    fix bash history

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 # Script written by @anatolethien
 
-echo -e '\n# Prompt\nfunction prompt() {\n\tPS1=" \\e[1;36m\\w\\e[m\\n `if [ \$? -eq 0 ]; then echo "\\e[1;32m"; else echo "\\e[1;31m"; fi`❱\\e[m ";\n}\nPROMPT_COMMAND=prompt' >> ~/.bashrc
+echo -e '\n# Prompt\nfunction prompt() {\n\tPS1=" \\[\\e[1;36m\\]\\w\\[\\e[m\\]\\n `if [ \$? -eq 0 ]; then echo "\\[\\e[1;32m\\]"; else echo "\\[\\e[1;31m\\]"; fi`❱\\[\\e[m\\] ";\n}\nPROMPT_COMMAND=prompt' >> ~/.bashrc
 source ~/.bashrc
