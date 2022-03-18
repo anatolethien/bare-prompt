ca486dc44ac32479ebd052996973adb54f6db29b
anatolethien <mail@anatolethien.eu>
2022-03-18 20:37:05 +0100

    python venv

--- prompt.sh
+++ prompt.sh
@@ -1,23 +1,24 @@
 
 # Prompt
 function prompt() {
+	local status=$?
 	local red="\[\e[1;31m\]"
 	local green="\[\e[1;32m\]"
 	local cyan="\[\e[1;36m\]"
 	local reset="\[\e[m\]"
-	if [[ -z $VIRTUAL_ENV ]]
+	if [ -z $VIRTUAL_ENV ]
 	then
 		local venv=""
 	else
 		local venv_name=$(echo $VIRTUAL_ENV | awk -F "/" '{print $NF}')
 		local venv="$green($venv_name)$reset "
 	fi
-	if [[ $? -eq 0 ]]
+	if [ $status -eq 0 ]
 	then
-		local error_color=$green
+		local status_color=$green
 	else
-		local error_color=$red
+		local status_color=$red
 	fi
-	PS1=" $venv$cyan\w$reset\n $error_color$reset "
+	PS1=" $venv$cyan\w$reset\n $status_color❱$reset "
 }
 PROMPT_COMMAND=prompt
