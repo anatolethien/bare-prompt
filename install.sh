e631f2e2af0ed7f00f0d04d54f53621a28794e55
anatolethien <mail@anatolethien.eu>
2023-03-29 12:06:39 +0200

    theme choice

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Script written by @anatolethien
 
-curl -sSL 'https://raw.githubusercontent.com/anatolethien/bare-prompt/master/prompt.sh' >> ~/.bashrc
+if [ -z "$1" ]; then
+  theme="main"
+else
+  theme="$1"
+fi
+
+curl -sSL "https://raw.githubusercontent.com/anatolethien/bare-prompt/master/themes/bare-prompt-$theme.sh" >> ~/.bashrc
 source ~/.bashrc
